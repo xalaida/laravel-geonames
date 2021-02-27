0d5c245d6228354e1414a18f1f72fd8d2b6272c9
nevadskiy <nevadskiy@gmail.com>
2021-02-27 20:43:26 +0200

    Add continents table migration

---
+++ database/migrations/2020_06_06_100000_create_continents_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Nevadskiy\Geonames\Models\Continent;
+
+class CreateContinentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(Continent::TABLE, static function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('code', 2)->unique();
+            $table->string('name', 50);
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
+            $table->string('timezone_id', 32)->nullable()->index();
+            $table->bigInteger('population')->unsigned()->nullable();
+            $table->smallInteger('elevation')->unsigned()->nullable()->comment('In meters.');
+            $table->smallInteger('dem')->nullable()->comment('Digital elevation model, srtm3 or gtopo30.');
+            $table->string('feature_code', 10)->nullable()->comment('See: https://www.geonames.org/export/codes.html');
+            $table->integer('geoname_id')->unsigned()->unique()->comment('Geonames database identifier.');
+            $table->date('modified_at')->comment('Date of last modification in the geonames database.');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(Continent::TABLE);
+    }
+}
