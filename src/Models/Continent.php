ad2a61e47388aadd45aec924ddb946c1f63bd128
nevadskiy <nevadskiy@gmail.com>
2021-02-27 20:42:59 +0200

    Add continent model

---
+++ src/Models/Continent.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Nevadskiy\Geonames\Models;
+
+use Carbon\CarbonTimeZone;
+use Illuminate\Support\Carbon;
+use Nevadskiy\Geonames\Services\ContinentCodeGenerator;
+use Nevadskiy\Geonames\ValueObjects\Location;
+use Nevadskiy\Geonames\Support\Eloquent\Model;
+
+/**
+ * @property string id
+ * @property string slug
+ * @property string code
+ * @property string name
+ * @property float latitude
+ * @property float longitude
+ * @property string timezone_id
+ * @property int population
+ * @property int dem
+ * @property int geoname_id
+ * @property Carbon modified_at
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ */
+class Continent extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    public const TABLE = 'continents';
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'modified_at' => 'date',
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    protected static function booted(): void
+    {
+        self::creating(static function (self $continent) {
+            $continent->code = (new ContinentCodeGenerator())->generate($continent);
+        });
+    }
+
+    /**
+     * Get the timezone instance.
+     */
+    public function getTimezone(): CarbonTimeZone
+    {
+        return new CarbonTimeZone($this->timezone_id);
+    }
+
+    /**
+     * Get the location instance.
+     */
+    public function getLocation(): Location
+    {
+        return new Location($this->latitude, $this->longitude);
+    }
+}
