2517ea4ad32799f59e19a602b007f754b50cf5b1
nevadskiy <nevadskiy@gmail.com>
2021-02-27 20:04:45 +0200

    Add base geoname model

---
+++ src/Support/Eloquent/Model.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Nevadskiy\Geonames\Support\Eloquent;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Nevadskiy\Uuid\Uuid;
+
+/**
+ * @property string id
+ */
+abstract class Model extends EloquentModel
+{
+    use HasFactory,
+        Uuid;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array|bool
+     */
+    protected $guarded = [];
+
+    /**
+     * Get the table associated with the model.
+     */
+    public function getTable(): string
+    {
+        if (defined('static::TABLE')) {
+            return static::TABLE;
+        }
+
+        return parent::getTable();
+    }
+}
