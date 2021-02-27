218ee33478ba6ced1281ab8b9ca921589b833318
nevadskiy <nevadskiy@gmail.com>
2021-02-27 20:41:16 +0200

    Add continent code generator

---
+++ src/Services/ContinentCodeGenerator.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Nevadskiy\Geonames\Services;
+
+use Nevadskiy\Geonames\Models\Continent;
+
+class ContinentCodeGenerator
+{
+    /**
+     * Generate a slug for the given continent.
+     *
+     * @param Continent $continent
+     * @return string
+     */
+    public function generate(Continent $continent): string
+    {
+        if (! str_contains($continent->name, ' ')) {
+            return $this->format($continent->name);
+        }
+
+        return $this->format($this->getAbbreviation($continent));
+    }
+
+    /**
+     * Format the given slug.
+     */
+    private function format(string $slug): string
+    {
+        return strtoupper(substr($slug, 0, 2));
+    }
+
+    /**
+     * Get an abbreviation of the continent.
+     *
+     * @param Continent $continent
+     */
+    private function getAbbreviation(Continent $continent): string
+    {
+        $slug = '';
+
+        foreach (explode(' ', $continent->name) as $part) {
+            $slug .= $part[0];
+        }
+
+        return $slug;
+    }
+}
