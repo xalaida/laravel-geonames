912ff6b6be8c13fb93264ed8d44598b682ebcb44
nevadskiy <nevadskiy@gmail.com>
2021-02-27 21:06:38 +0200

    Add city model and migration

---
+++ database/migrations/2020_06_06_400000_create_cities_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Nevadskiy\Geonames\Models\City;
+use Nevadskiy\Geonames\Models\Country;
+use Nevadskiy\Geonames\Models\Division;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(City::TABLE, static function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->foreignUuid('country_id')->references('id')->on(Country::TABLE)->cascadeOnDelete();
+            $table->foreignUuid('division_id')->nullable()->references('id')->on(Division::TABLE)->cascadeOnDelete();
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
+            $table->string('timezone_id', 32)->nullable()->index();
+            $table->bigInteger('population')->unsigned()->nullable();
+            $table->smallInteger('elevation')->unsigned()->nullable()->comment('In meters.');
+            $table->smallInteger('dem')->nullable()->comment('Digital elevation model, srtm3 or gtopo30.');
+            $table->string('feature_code', 10)->nullable()->comment('See: https://www.geonames.org/export/codes.html');
+            $table->integer('geoname_id')->unsigned()->unique()->comment('Geonames database identifier.');
+            $table->date('modified_at')->comment('Date of last modification in the geonames database.');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(City::TABLE);
+    }
+}
---
+++ src/Models/City.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Nevadskiy\Geonames\Models;
+
+use Carbon\CarbonTimeZone;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
+use Nevadskiy\Geonames\ValueObjects\Location;
+use Nevadskiy\Geonames\Support\Eloquent\Model;
+use Nevadskiy\Translatable\HasTranslations;
+
+/**
+ * @property string id
+ * @property string name
+ * @property string country_id
+ * @property float latitude
+ * @property float longitude
+ * @property string|null timezone_id
+ * @property integer|null population
+ * @property integer|null elevation
+ * @property integer|null dem
+ * @property string feature_code
+ * @property int geoname_id
+ * @property Carbon modified_at
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ */
+class City extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    public const TABLE = 'cities';
+
+    /**
+     * The attributes that can be translatable.
+     *
+     * @var array
+     */
+    protected $translatable = [
+        'name',
+    ];
+
+    /**
+     * Get the timezone instance.
+     */
+    public function getTimezone(): ?CarbonTimeZone
+    {
+        if (! $this->timezone_id) {
+            return null;
+        }
+
+        return new CarbonTimeZone($this->timezone_id);
+    }
+
+    /**
+     * Get the location instance.
+     */
+    public function getLocation(): Location
+    {
+        return new Location($this->latitude, $this->longitude);
+    }
+
+    /**
+     * Get a country of the city.
+     */
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class, 'country_id');
+    }
+
+    /**
+     * Get a division of the city.
+     */
+    public function division(): BelongsTo
+    {
+        return $this->belongsTo(Division::class, 'division_id');
+    }
+}
