70f869d22636898f1254345b6ced09be830e29eb
nevadskiy <nevadskiy@gmail.com>
2021-02-28 14:41:57 +0200

    Update doc block

--- src/Support/FileDownloader/Downloader.php
+++ src/Support/FileDownloader/Downloader.php
@@ -5,12 +5,12 @@ namespace Nevadskiy\Geonames\Support\FileDownloader;
 interface Downloader
 {
     /**
-     * Download a file by the given url.
+     * Download a file by the given url and returns the final path.
      *
      * @param string $url
      * @param string $directory
      * @param string|null $name
-     * @return string realpath
+     * @return string
      */
     public function download(string $url, string $directory, string $name = null): string;
 }
