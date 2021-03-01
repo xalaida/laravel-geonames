1aec81f8aa58db6d64dce0b23223117ff2a296e1
nevadskiy <nevadskiy@gmail.com>
2021-03-01 21:08:04 +0200

    Fix downloader events

--- src/Support/FileDownloader/ConsoleFileDownloader.php
+++ src/Support/FileDownloader/ConsoleFileDownloader.php
@@ -3,9 +3,17 @@
 namespace Nevadskiy\Geonames\Support\FileDownloader;
 
 use Illuminate\Console\OutputStyle;
+use Symfony\Component\Console\Helper\ProgressBar;
 
 class ConsoleFileDownloader extends FileDownloader
 {
+    /**
+     * The console progress bar.
+     *
+     * @var ProgressBar
+     */
+    protected $progress;
+
     /**
      * Enable the console progress bar.
      *
@@ -15,18 +23,19 @@ class ConsoleFileDownloader extends FileDownloader
     public function enableProgressBar(OutputStyle $output): self
     {
         $this->onReady(function (int $steps, string $url) use ($output) {
-            $progress = $output->createProgressBar($steps);
-            $progress->setFormat("<info>Downloading:</info> {$url}\n%bar% %percent%%\n<info>Remaining Time:</info> %remaining%");
+            $this->progress = $output->createProgressBar($steps);
+            $this->progress->setFormat("<info>Downloading:</info> {$url}\n%bar% %percent%%\n<info>Remaining Time:</info> %remaining%");
 
-            $this->onStep(static function () use ($progress) {
-                $progress->advance();
-            });
+        });
 
-            $this->onFinish(static function (string $path) use ($progress, $output) {
-                $progress->finish();
-                $output->newLine();
-                $output->writeln("<info>File Downloaded:</info> {$path}");
-            });
+        $this->onStep(function () {
+            $this->progress->advance();
+        });
+
+        $this->onFinish(function (string $path) use ($output) {
+            $this->progress->finish();
+            $output->newLine();
+            $output->writeln("<info>File Downloaded:</info> {$path}");
         });
 
         return $this;
