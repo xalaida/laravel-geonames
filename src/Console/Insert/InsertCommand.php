f68eb3e2bdf49b52a8217aa7c948cdd984bbebca
nevadskiy <nevadskiy@gmail.com>
2021-03-13 14:32:00 +0200

    Add without translations option

--- src/Console/Insert/InsertCommand.php
+++ src/Console/Insert/InsertCommand.php
@@ -11,7 +11,6 @@ use Nevadskiy\Geonames\Geonames;
 use Nevadskiy\Geonames\Services\DownloadService;
 use Nevadskiy\Geonames\Services\SupplyService;
 use Nevadskiy\Geonames\Services\TranslateService;
-use Nevadskiy\Geonames\Support\Downloader\ConsoleDownloader;
 use Nevadskiy\Geonames\Support\Downloader\Downloader;
 
 class InsertCommand extends Command
@@ -23,7 +22,7 @@ class InsertCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'geonames:insert {--truncate} {--update-files}';
+    protected $signature = 'geonames:insert {--truncate} {--update-files} {--without-translations}';
 
     /**
      * The console command description.
@@ -79,14 +78,71 @@ class InsertCommand extends Command
     ): void
     {
         $this->init($geonames, $dispatcher, $downloadService, $supplyService, $translateService);
-
-        $this->info('Start inserting geonames dataset.');
         $this->dispatcher->dispatch(new GeonamesCommandReady());
+        $this->setUpDownloader($this->downloadService->getDownloader());
 
         $this->insert();
         $this->translate();
+    }
+
+    /**
+     * Init the command instance with all required services.
+     */
+    private function init(
+        Geonames $geonames,
+        Dispatcher $dispatcher,
+        DownloadService $downloadService,
+        SupplyService $supplyService,
+        TranslateService $translateService
+    ): void
+    {
+        $this->geonames = $geonames;
+        $this->dispatcher = $dispatcher;
+        $this->downloadService = $downloadService;
+        $this->supplyService = $supplyService;
+        $this->translateService = $translateService;
+    }
+
+    /**
+     * Insert the geonames dataset.
+     */
+    private function insert(): void
+    {
+        $this->info('Start inserting geonames dataset.');
+
+        $this->truncate();
+
+        if ($this->geonames->shouldSupplyCountries()) {
+            $this->info('Add country info.');
+            $this->supplyService->addCountryInfo($this->downloadService->downloadCountryInfoFile());
+        }
+
+        foreach ($this->downloadService->downloadSourceFiles() as $path) {
+            $this->info("Processing the {$path} file.");
+            $this->supplyService->insert($path);
+        }
 
         $this->info('Geonames dataset has been successfully inserted.');
+    }
+
+    /**
+     * Translate inserted data.
+     */
+    private function translate(): void
+    {
+        if ($this->option('without-translations')) {
+            return;
+        }
+
+        $this->info('Start seeding translations. It may take some time.');
+
+        // TODO: refactor with config and translatable package features.
+        DB::table('translations')->truncate();
+
+        // TODO: feature downloading specific alternate names source (single country or everything)
+        $this->translateService->insert($this->downloadService->downloaderAlternateNames());
+
+        $this->info('Translations have been successfully seeded.');
     }
 
     /**
@@ -127,26 +183,6 @@ class InsertCommand extends Command
     }
 
     /**
-     * Init the command instance with all required services.
-     */
-    private function init(
-        Geonames $geonames,
-        Dispatcher $dispatcher,
-        DownloadService $downloadService,
-        SupplyService $supplyService,
-        TranslateService $translateService
-    ): void
-    {
-        $this->geonames = $geonames;
-        $this->dispatcher = $dispatcher;
-        $this->downloadService = $downloadService;
-        $this->supplyService = $supplyService;
-        $this->translateService = $translateService;
-
-        $this->setUpDownloader($this->downloadService->getDownloader());
-    }
-
-    /**
      * Set up the console downloader.
      *
      * @param Downloader $downloader
@@ -156,38 +192,5 @@ class InsertCommand extends Command
         if ($this->option('update-files')) {
             $downloader->update();
         }
-    }
-
-    /**
-     * Insert the geonames dataset.
-     */
-    private function insert(): void
-    {
-        $this->truncate();
-
-        if ($this->geonames->shouldSupplyCountries()) {
-            $this->info('Add country info.');
-            $this->supplyService->addCountryInfo($this->downloadService->downloadCountryInfoFile());
-        }
-
-        foreach ($this->downloadService->downloadSourceFiles() as $path) {
-            $this->info("Processing the {$path} file.");
-            $this->supplyService->insert($path);
-        }
-    }
-
-    /**
-     * Translate inserted data.
-     */
-    private function translate(): void
-    {
-        $this->info('Start seeding translations. It may take some time.');
-
-        // TODO: refactor with config and translatable package features.
-        DB::table('translations')->truncate();
-
-        $this->translateService->insert($this->downloadService->downloaderAlternateNames());
-
-        $this->info('Translations have been successfully seeded.');
     }
 }
