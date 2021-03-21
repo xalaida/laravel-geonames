1e868a5fa6cced65b8d3a336c3c68335b9adea65
nevadskiy <nevadskiy@gmail.com>
2021-03-21 19:58:05 +0200

    Add verbosity level

--- src/Support/Output/OutputFactory.php
+++ src/Support/Output/OutputFactory.php
@@ -4,15 +4,23 @@ namespace Nevadskiy\Geonames\Support\Output;
 
 use Illuminate\Console\OutputStyle;
 use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\StreamOutput;
 
 class OutputFactory
 {
     /**
      * Make the output style.
+     *
+     * @param int $verbosity
+     * @return OutputStyle
      */
-    public static function make(): OutputStyle
+    public static function make($verbosity = OutputInterface::VERBOSITY_NORMAL): OutputStyle
     {
-        return new OutputStyle(new StringInput(''), new StreamOutput(fopen('php://stdout', 'wb')));
+        $output = new OutputStyle(new StringInput(''), new StreamOutput(fopen('php://stdout', 'wb')));
+
+        $output->setVerbosity($verbosity);
+
+        return $output;
     }
 }
