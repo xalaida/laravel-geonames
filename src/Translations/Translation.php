7c3170224fce358ab8a651dc4c72f3574afb2fce
nevadskiy <nevadskiy@gmail.com>
2022-05-25 00:35:14 +0300

    Add base translation model

---
+++ src/Translations/Translation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Nevadskiy\Geonames\Translations;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Translation extends Model
+{
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_preferred' => 'boolean',
+        'is_short' => 'boolean',
+        'is_colloquial' => 'boolean',
+        'is_historic' => 'boolean',
+    ];
+
+    // TODO: probably extend AdditionalTable strategy model.
+    // TODO: add some scopes for translation priority.
+    // TODO: make model use this trait
+}
