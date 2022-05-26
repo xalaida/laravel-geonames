9fa81a5aa92d2f9dc3657e24c57cf75678848c06
nevadskiy <nevadskiy@gmail.com>
2022-05-26 13:33:51 +0300

    Feature model syncing

--- src/Seeders/ModelSeeder.php
+++ src/Seeders/ModelSeeder.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use DateTimeInterface;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\LazyCollection;
 
 /**
@@ -13,7 +14,6 @@ use Illuminate\Support\LazyCollection;
  */
 abstract class ModelSeeder implements Seeder
 {
-    use SyncsModelRecords;
     use DailyUpdateModelRecords;
     use DailyDeleteModelRecords;
 
@@ -161,6 +161,121 @@ abstract class ModelSeeder implements Seeder
      * Map fields to the model attributes.
      */
     abstract protected function mapAttributes(array $record): array;
+
+    /**
+     * Get records for syncing database.
+     */
+    protected function getRecordsForSyncing(): iterable
+    {
+        return $this->getRecordsForSeeding();
+    }
+
+    /**
+     * Sync database according to the dataset.
+     *
+     * @TODO: log report.
+     */
+    public function sync(): void
+    {
+        $this->resetSyncedModels();
+
+        $updatable = $this->getUpdatableAttributes();
+
+        foreach ($this->getRecordsForSeeding()->chunk(1000) as $chunk) {
+            $this->query()->upsert($chunk->all(), [self::SYNC_KEY], $updatable);
+        }
+
+        $this->deleteUnsyncedModels();
+    }
+
+    /**
+     * Get updatable attributes of the model.
+     */
+    protected function getUpdatableAttributes(): array
+    {
+        $updatable = $this->updatable();
+
+        if (! $this->isWildcardAttributes($updatable)) {
+            return $updatable;
+        }
+
+        return collect($this->getColumns())
+            ->diff(['id', self::SYNC_KEY, 'created_at'])
+            ->values()
+            ->all();
+    }
+
+    /**
+     * Determine if the given attributes is a wildcard.
+     */
+    protected function isWildcardAttributes(array $attributes): bool
+    {
+        return count($attributes) === 1 && $attributes[0] === '*';
+    }
+
+    /**
+     * Get the updatable attributes of the model.
+     */
+    protected function updatable(): array
+    {
+        return ['*'];
+    }
+
+    /**
+     * Get column list for the database model.
+     */
+    protected function getColumns(): array
+    {
+        return DB::connection()
+            ->getSchemaBuilder()
+            ->getColumnListing($this->newModel()->getTable());
+    }
+
+    /**
+     * Reset a "sync" state for database models.
+     */
+    protected function resetSyncedModels(int $chunk = 50000): void
+    {
+        while ($this->synced()->exists()) {
+            $this->synced()
+                ->toBase()
+                ->limit($chunk)
+                ->update([self::SYNCED_AT => null]);
+        }
+    }
+
+    /**
+     * Get a query for synced models.
+     */
+    protected function synced(): Builder
+    {
+        return $this->query()->whereNotNull(self::SYNCED_AT);
+    }
+
+    /**
+     * Delete unsynced models from database and return its amount.
+     *
+     * @TODO: add possibility to prevent models from being deleted... (probably use extended query with some scopes)
+     * @TODO: integrate with soft delete.
+     */
+    protected function deleteUnsyncedModels(): int
+    {
+        $deleted = 0;
+
+        while ($this->unsynced()->exists()) {
+            $deleted += $this->unsynced()->delete();
+        }
+
+        return $deleted;
+    }
+
+    /**
+     * Get a query for unsynced records.
+     */
+    protected function unsynced(): Builder
+    {
+        return $this->query()->whereNull(self::SYNCED_AT);
+    }
 
     /**
      * Truncate a table of the model.
