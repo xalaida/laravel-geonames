73d0c42300b92661f0a313c66f4126af00dcbcf8
nevadskiy <nevadskiy@gmail.com>
2022-07-11 23:44:58 +0300

    Add history downloader

---
+++ src/Downloader/HistoryDownloader.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Nevadskiy\Geonames\Downloader;
+
+use Nevadskiy\Downloader\Downloader;
+
+class HistoryDownloader implements Downloader
+{
+    /**
+     * The base downloader instance.
+     *
+     * @var Downloader
+     */
+    protected $downloader;
+
+    /**
+     * The downloader history.
+     *
+     * @var array
+     */
+    protected $history = [];
+
+    /**
+     * Make a new downloader instance.
+     */
+    public function __construct(Downloader $downloader)
+    {
+        $this->downloader = $downloader;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function download(string $url, string $destination): string
+    {
+        if (! isset($this->history[$url])) {
+            $this->history[$url] = $this->downloader->download($url, $destination);
+        }
+
+        return $this->history[$url];
+    }
+}
