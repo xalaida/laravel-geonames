eb748149c7f55bca5e77222ca25808edd130f8b0
nevadskiy <nevadskiy@gmail.com>
2022-07-16 23:48:46 +0300

    Remove old dependencies

--- src/GeonamesServiceProvider.php
+++ src/GeonamesServiceProvider.php
@@ -5,18 +5,6 @@ namespace Nevadskiy\Geonames;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
-use Nevadskiy\Geonames\Parsers\FileParser;
-use Nevadskiy\Geonames\Parsers\Parser;
-use Nevadskiy\Geonames\Parsers\ProgressParser;
-use Nevadskiy\Geonames\Support\Downloader\BaseDownloader;
-use Nevadskiy\Geonames\Support\Downloader\ConsoleDownloader;
-use Nevadskiy\Geonames\Support\Downloader\Downloader;
-use Nevadskiy\Geonames\Support\Downloader\UnzipperDownloader;
-use Nevadskiy\Geonames\Support\FileReader\BaseFileReader;
-use Nevadskiy\Geonames\Support\FileReader\FileReader;
-use Nevadskiy\Geonames\Support\Logger\ConsoleLogger;
-use Nevadskiy\Geonames\Support\Output\OutputFactory;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\Finder\SplFileInfo;
 
 class GeonamesServiceProvider extends ServiceProvider
@@ -27,10 +15,6 @@ class GeonamesServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->registerConfig();
-        $this->registerLogger();
-        $this->registerDownloader();
-        $this->registerFileReader();
-        $this->registerParser();
     }
 
     /**
@@ -51,62 +35,6 @@ class GeonamesServiceProvider extends ServiceProvider
     protected function registerConfig(): void
     {
         $this->mergeConfigFrom(__DIR__.'/../config/geonames.php', 'geonames');
-    }
-
-    /**
-     * Register any package logger.
-     */
-    protected function registerLogger(): void
-    {
-        $this->app->singleton(ConsoleLogger::class, function () {
-            return new ConsoleLogger(OutputFactory::make());
-        });
-
-        if ($this->app->runningInConsole()) {
-            $this->app->when(BaseDownloader::class)
-                ->needs(LoggerInterface::class)
-                ->give(ConsoleLogger::class);
-        }
-    }
-
-    /**
-     * Register any package downloader.
-     */
-    protected function registerDownloader(): void
-    {
-        $this->app->bind(Downloader::class, BaseDownloader::class);
-
-        $this->app->extend(Downloader::class, function (Downloader $downloader) {
-            return $this->app->make(UnzipperDownloader::class, ['downloader' => $downloader]);
-        });
-
-        if ($this->app->runningInConsole() && ! $this->app->runningUnitTests()) {
-            $this->app->extend(Downloader::class, function (Downloader $downloader) {
-                return new ConsoleDownloader($downloader, OutputFactory::make());
-            });
-        }
-    }
-
-    /**
-     * Register any package file reader.
-     */
-    protected function registerFileReader(): void
-    {
-        $this->app->bind(FileReader::class, BaseFileReader::class);
-    }
-
-    /**
-     * Register any package resource parser.
-     */
-    protected function registerParser(): void
-    {
-        $this->app->bind(Parser::class, FileParser::class);
-
-        if ($this->app->runningInConsole() && ! $this->app->runningUnitTests()) {
-            $this->app->extend(Parser::class, function (Parser $parser) {
-                return new ProgressParser($parser, OutputFactory::make());
-            });
-        }
     }
 
     /**
