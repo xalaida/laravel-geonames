565e91f813f0864849f7c0fb7788abfb8b8f9814
nevadskiy <nevadskiy@gmail.com>
2022-07-17 23:52:35 +0300

    Refactor download service

--- src/Services/DownloadService.php
+++ src/Services/DownloadService.php
@@ -6,37 +6,11 @@ use Carbon\Carbon;
 use InvalidArgumentException;
 use Nevadskiy\Downloader\Downloader;
 
-// TODO: add possibility to configure service from constructor for automatic downloads
+/**
+ * TODO: add alternateNamesV2
+ */
 class DownloadService
 {
-    /**
-     * The all countries source.
-     * It's a huge source that contains everything, unzipped size is about 1.5GB.
-     *
-     * Source URL: http://download.geonames.org/export/dump/allCountries.zip
-     */
-    public const SOURCE_ALL_COUNTRIES = 'all_countries';
-
-    /**
-     * The single country source.
-     * Use it when you need dataset that belongs to one or multiple countries.
-     * Continents table is not available with this source.
-     * The country codes must be specified as countries filter configuration.
-     *
-     * Example of source URL for the US: http://download.geonames.org/export/dump/US.zip
-     */
-    public const SOURCE_SINGLE_COUNTRY = 'single_country';
-
-    /**
-     * The only cities source.
-     * Use it when you only need the cities table.
-     * Continents, countries and divisions is not available with this source.
-     * Also, you need to specify population filter for the specific source file.
-     *
-     * Example of source URL for cities with population above 15000: http://download.geonames.org/export/dump/cities15000.zip
-     */
-    public const SOURCE_ONLY_CITIES = 'only_cities';
-
     /**
      * The downloader instance.
      *
@@ -54,272 +28,11 @@ class DownloadService
     /**
      * DownloadService constructor.
      */
-    public function __construct(Downloader $downloader)
+    public function __construct(Downloader $downloader, string $directory)
     {
         $this->downloader = $downloader;
         // TODO: inject "directory" from service provider
-        $this->directory = config('geonames.directory');
-    }
-
-    /**
-     * Download geonames source files.
-     */
-    public function downloadSourceFiles(): array
-    {
-        $paths = [];
-
-        foreach ($this->getSourceUrls() as $url) {
-            $paths[] = $this->download($url);
-        }
-
-        return $paths;
-    }
-
-    /**
-     * Download geonames alternate names resources.
-     */
-    public function downloaderAlternateNames(): array
-    {
-        $paths = [];
-
-        foreach ($this->getAlternateNamesUrl() as $url) {
-            $paths[] = $this->download($url);
-        }
-
-        return $paths;
-    }
-
-    /**
-     * Download alternate names file.
-     */
-    public function downloadAlternateNames(): string
-    {
-        $directory = $this->download($this->getAllAlternateNamesUrl());
-
-        return $directory.DIRECTORY_SEPARATOR.'alternateNames.txt';
-    }
-
-    /**
-     * Download geonames daily modifications file.
-     */
-    public function downloadDailyModifications(): string
-    {
-        return $this->download($this->getDailyModificationsUrl());
-    }
-
-    /**
-     * Download geonames daily deletes file.
-     */
-    public function downloadDailyDeletes(): string
-    {
-        return $this->download($this->getDailyDeletesUrl());
-    }
-
-    /**
-     * Download geonames daily alternate name modifications file.
-     */
-    public function downloadDailyAlternateNamesModifications(): string
-    {
-        // TODO: return correct file.
-
-        return $this->download($this->getDailyAlternateNamesModificationsUrl());
-    }
-
-    /**
-     * Download geonames daily alternate name deletes file.
-     */
-    public function downloadDailyAlternateNamesDeletes(): string
-    {
-        // TODO: return correct file.
-
-        return $this->download($this->getDailyAlternateNamesDeletesUrl());
-    }
-
-    /**
-     * Download geonames country info file.
-     */
-    public function downloadCountryInfo(): string
-    {
-        // TODO: return correct file.
-
-        return $this->download($this->getCountryInfoUrl());
-    }
-
-    /**
-     * Download the all countries file.
-     */
-    public function downloadAllCountries(): string
-    {
-        // TODO: consider unzipping files here (since the download service assume that file is going to be a ZIP archive)
-
-        $directory = $this->download($this->getAllCountriesUrl());
-
-        return $directory.DIRECTORY_SEPARATOR.'allCountries.txt';
-    }
-
-    /**
-     * Perform the downloading process.
-     *
-     * @todo fix
-     * @return array|string
-     */
-    protected function download(string $url)
-    {
-        return $this->downloader->download($url, $this->directory);
-    }
-
-    /**
-     * Get the URLs of the geonames sources.
-     *
-     * @todo fix
-     */
-    protected function getSourceUrls(): array
-    {
-        if ($this->geonames->isOnlyCitiesSource()) {
-            return [$this->getCitiesUrl($this->geonames->getPopulation())];
-        }
-
-        if ($this->geonames->isSingleCountrySource()) {
-            return $this->getSingleCountryUrls($this->geonames->getCountries());
-        }
-
-        return [$this->getAllCountriesUrl()];
-    }
-
-    /**
-     * Get the URL of the cities file with the given population.
-     */
-    protected function getCitiesUrl(int $population): string
-    {
-        $this->assertAvailablePopulation($population);
-
-        return "{$this->getBaseUrl()}cities{$population}.zip";
-    }
-
-    /**
-     * Get the URL of the single country file by the given country codes.
-     */
-    protected function getSingleCountryUrls(array $countries): array
-    {
-        $this->assertCountryIsSpecified($countries);
-
-        $urls = [];
-
-        foreach ($countries as $country) {
-            $urls[] = $this->getSingleCountryUrl($country);
-        }
-
-        return $urls;
-    }
-
-    /**
-     * Get the URLs of the single country of the alternate names files by the given country codes.
-     */
-    protected function getSingleCountryAlternateNamesUrls(array $countries): array
-    {
-        $this->assertCountryIsSpecified($countries);
-
-        $urls = [];
-
-        foreach ($countries as $country) {
-            $urls[] = $this->getSingleCountryAlternateNamesUrl($country);
-        }
-
-        return $urls;
-    }
-
-    /**
-     * Get the country info resource URL.
-     */
-    protected function getCountryInfoUrl(): string
-    {
-        return "{$this->getBaseUrl()}countryInfo.txt";
-    }
-
-    /**
-     * Get the geonames alternate names resource URLs.
-     *
-     * @todo fix
-     */
-    protected function getAlternateNamesUrl(): array
-    {
-        if ($this->geonames->isSingleCountrySource()) {
-            return $this->getSingleCountryAlternateNamesUrls($this->geonames->getCountries());
-        }
-
-        return [$this->getAllAlternateNamesUrl()];
-    }
-
-    /**
-     * Get the all countries geonames resource URL.
-     */
-    protected function getAllCountriesUrl(): string
-    {
-        return "{$this->getBaseUrl()}allCountries.zip";
-    }
-
-    /**
-     * Get the URL of the single country file by the given country code.
-     */
-    protected function getSingleCountryUrl(string $code): string
-    {
-        return "{$this->getBaseUrl()}{$code}.zip";
-    }
-
-    /**
-     * Get the URL of the single country of the alternate name file by the given country code.
-     */
-    protected function getSingleCountryAlternateNamesUrl(string $code): string
-    {
-        return "{$this->getBaseUrl()}alternatenames/{$code}.zip";
-    }
-
-    /**
-     * Get the previous date of geonames updates.
-     */
-    protected function getGeonamesLastUpdateDate(): Carbon
-    {
-        return Carbon::yesterday('UTC');
-    }
-
-    /**
-     * Get the URL of the geonames daily modifications file.
-     */
-    protected function getDailyModificationsUrl(): string
-    {
-        return $this->getDailyUpdateUrlByType('modifications');
-    }
-
-    /**
-     * Get the URL of the geonames daily deletes file.
-     */
-    protected function getDailyDeletesUrl(): string
-    {
-        return $this->getDailyUpdateUrlByType('deletes');
-    }
-
-    /**
-     * Get the URL of the geonames daily alternate names modifications file.
-     */
-    protected function getDailyAlternateNamesModificationsUrl(): string
-    {
-        return $this->getDailyUpdateUrlByType('alternateNamesModifications');
-    }
-
-    /**
-     * Get the URL of the geonames daily alternate names deletes file.
-     */
-    protected function getDailyAlternateNamesDeletesUrl(): string
-    {
-        return $this->getDailyUpdateUrlByType('alternateNamesDeletes');
-    }
-
-    /**
-     * Get the URL of the geonames daily deletes file.
-     */
-    protected function getDailyUpdateUrlByType(string $type): string
-    {
-        return "{$this->getBaseUrl()}{$type}-{$this->getGeonamesLastUpdateDate()->format('Y-m-d')}.txt";
+        $this->directory = $directory ?: config('geonames.directory');
     }
 
     /**
@@ -331,27 +44,139 @@ class DownloadService
     }
 
     /**
+     * Perform the download process.
+     */
+    public function download(string $url): string
+    {
+        return $this->downloader->download($url, $this->directory);
+    }
+
+    /**
+     * Download a ZIP archive and return the main file from the archive.
+     */
+    public function downloadZip(string $url): string
+    {
+        return $this->download($url).DIRECTORY_SEPARATOR.pathinfo($url, PATHINFO_FILENAME);
+    }
+
+    /**
+     * Download the geonames country info file.
+     */
+    public function downloadCountryInfo(): string
+    {
+        return $this->download("{$this->getBaseUrl()}countryInfo.txt");
+    }
+
+    /**
+     * Download the all countries file.
+     */
+    public function downloadAllCountries(): string
+    {
+        return $this->downloadZip("{$this->getBaseUrl()}allCountries.zip");
+    }
+
+    /**
+     * Download a single country file by the given country code.
+     */
+    public function downloadSingleCountry(string $country): string
+    {
+        return $this->downloadZip("{$this->getBaseUrl()}{$country}.zip");
+    }
+
+    /**
+     * Download an alternate names file.
+     */
+    public function downloadAlternateNames(): string
+    {
+        return $this->downloadZip("{$this->getBaseUrl()}alternateNames.zip");
+    }
+
+    /**
+     * Download an alternate names file of a single country by the given country code.
+     */
+    public function downloadSingleCountryAlternateNames(string $country): string
+    {
+        return $this->downloadZip("{$this->getBaseUrl()}alternatenames/{$country}.zip");
+    }
+
+    /**
+     * Get the URL of the geonames daily deletes file.
+     */
+    protected function getDailyUpdateUrlByType(string $type): string
+    {
+        return "{$this->getBaseUrl()}{$type}-{$this->getGeonamesLastUpdateDate()->format('Y-m-d')}.txt";
+    }
+
+    /**
+     * Get the previous date of geonames updates.
+     */
+    protected function getGeonamesLastUpdateDate(): Carbon
+    {
+        return Carbon::yesterday('UTC');
+    }
+
+    /**
+     * Download geonames daily modifications file.
+     */
+    public function downloadDailyModifications(): string
+    {
+        return $this->download($this->getDailyUpdateUrlByType('modifications'));
+    }
+
+    /**
+     * Download geonames daily deletes file.
+     */
+    public function downloadDailyDeletes(): string
+    {
+        return $this->download($this->getDailyUpdateUrlByType('deletes'));
+    }
+
+    /**
+     * Download geonames daily alternate name modifications file.
+     */
+    public function downloadDailyAlternateNamesModifications(): string
+    {
+        return $this->download($this->getDailyUpdateUrlByType('alternateNamesModifications'));
+    }
+
+    /**
+     * Download geonames daily alternate name deletes file.
+     */
+    public function downloadDailyAlternateNamesDeletes(): string
+    {
+        return $this->download($this->getDailyUpdateUrlByType('alternateNamesDeletes'));
+    }
+
+    /**
+     * Download a "cities" file by the given population.
+     */
+    public function downloadCities(int $population): string
+    {
+        $this->ensurePopulationAvailable($population);
+
+        return $this->downloadZip("{$this->getBaseUrl()}cities{$population}.zip");
+    }
+
+    /**
+     * Download a file with no country related locations.
+     */
+    public function downloadNoCountry(): string
+    {
+        return $this->downloadZip("{$this->getBaseUrl()}no-country.zip");
+    }
+
+    /**
      * Assert that the given population is available to download.
      */
-    protected function assertAvailablePopulation(int $population): void
+    protected function ensurePopulationAvailable(int $population): void
     {
         if (! in_array($population, $this->getPopulations())) {
             throw new InvalidArgumentException(
-                vsprintf('There is no file with %s population. Specify one of %s', [
+                vsprintf('There is no file with "%s" population. Specify one of: %s', [
                     $population,
                     implode(', ', $this->getPopulations()),
                 ])
             );
-        }
-    }
-
-    /**
-     * Assert that a country code is specified.
-     */
-    protected function assertCountryIsSpecified(array $countries): void
-    {
-        if ($countries === ['*']) {
-            throw new InvalidArgumentException('Specify a country code in the geonames configuration file.');
         }
     }
 
@@ -368,13 +193,5 @@ class DownloadService
             5000,
             15000,
         ];
-    }
-
-    /**
-     * Get full alternate names URL.
-     */
-    protected function getAllAlternateNamesUrl(): string
-    {
-        return "{$this->getBaseUrl()}alternateNames.zip";
     }
 }
