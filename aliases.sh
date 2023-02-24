d133d2cda5481f51f867c1b1912214e54c9fc2ae
nevadskiy <nevadskiy@gmail.com>
2023-02-24 18:21:46 +0200

    Remove old files

--- aliases.sh
+++
@@ -1,8 +0,0 @@
-# Run command from the app container
-alias app='docker-compose run --rm app'
-
-# Run the testsuite with a filter option
-alias tf='docker-compose run --rm app vendor/bin/phpunit --filter'
-
-# Run the testsuite with a filter option and coverage enabled
-alias tfc='docker-compose run --rm app vendor/bin/phpunit --coverage-text --filter'
